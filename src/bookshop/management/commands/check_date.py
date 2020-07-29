eb0dc64511253655148686a749c4d3934d6fa191
Bogdan <bogdan@mail.ru>
2020-07-29 21:38:04 +0300

    parse holiday and create holiday model

--- src/bookshop/management/commands/check_date.py
+++ src/bookshop/management/commands/check_date.py
@@ -3,12 +3,27 @@ from bookshop.models import Event
 from logging import getLogger
 from datetime import datetime
 from django.core.mail import send_mail
+import pytz
 
 
+utc = pytz.UTC
 logger = getLogger("django")
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        datetime.now()
-        logger.warning("works")
+        now = utc.localize(datetime.now())
+        all_events = Event.objects.all()
+        for event in all_events:
+            if event.need_remind and event.date_start - event.reminder <= now:
+                email = event.user_event.email
+                send_mail(
+                    "holiday remainder",
+                    f"{event.title} - {event.date_start}",
+                    "akademiynauk3@gmail.com",
+                    [email],
+                    fail_silently=True
+                )
+                event.need_remind = False
+                event.save()
+                logger.warning("sent")
